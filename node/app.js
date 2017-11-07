40a958334bf24a1924e63e446eaec4d6a2524973
Daniel <danielsusser@hotmail.co.uk>
2017-11-07 23:15:37 +0000

    server setup

--- node/app.js
+++ node/app.js
@@ -1,7 +1,24 @@
-var http = require('http');
-var fs = require('fs');
+var express = require('express');
+var plantrController = require('./controllers/PlantrController');
 
-var server = http.createServer(function(req,res){
+//starts the server
+var app = express();
+
+//sets up template engine (do I even need that? who knows...)
+app.set('view engine','ejs');
+
+//use static files
+app.use(express.static('.'));
+
+//listen to port 3000
+app.listen(3000);
+
+//if everything's working log a message
+console.log('listening to port 3000');
+
+plantrController(app);
+//
+/*var server = http.createServer(function(req,res){
   console.log('request was made: '+req.url);
   if(req.url === '/home' || req.url === '/'){
     res.writeHead(200,{'Content-Type':'text/html'});
@@ -16,4 +33,5 @@ var server = http.createServer(function(req,res){
 });
 
 server.listen(3000,'127.0.0.1');
-console.log('listening');
+console.log('listening to port 3000');
+*/
